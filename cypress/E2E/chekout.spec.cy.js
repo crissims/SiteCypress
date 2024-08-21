0921ecc6e8e9ffa4f9e122bc235337ca5820275f
Ana Leal <ana.leal@globalsys.com.br>
2024-08-21 19:05:16 -0300

    novas alterações

--- cypress/E2E/chekout.spec.cy.js
+++ cypress/E2E/chekout.spec.cy.js
@@ -5,21 +5,21 @@ describe('Fluxo de compra', () => {
     });
     it('Realizar uma compra completa', () => {
  
-        cy.get('.vtex-rich-text-0-x-wrapper--menuLoginIcon').click()
-        cy.contains('Entrar com email e senha').click()
+        cy.get('.vtex-rich-text-0-x-wrapper--menuLoginIcon').click()  //clicando no botão de login
+        cy.contains('Entrar com email e senha').click()  
         cy.get('[name="userEmail"]').type('ana.leal@globalsys.com.br')
-        cy.get('.containerPassword > .undefined').type('Teste@reserva123')
+        cy.get('.containerPassword > .undefined').type('Teste@reserva123') 
         cy.get('.sendButton').click()
-        cy.get(':nth-child(2) > .rsv__header__nav__list__item').click()
-        cy.get('a[href="/colecao-reversa/feminino-camisas"]').click()
+        cy.get(':nth-child(2) > .rsv__header__nav__list__item').click() //criando na categoria feminina
+        cy.get('a[href="/colecao-reversa/feminino-camisas"]').click() // entrando na coleção camisas
         cy.wait(30000)
-        cy.contains('Camisa Sarja Cropped Color').click({waitForAnimations: false})
-        cy.wait(30000)
-        cy.get('.lojausereserva-add-to-cart-button-0-x-buttonText').click({ force: true }); 
+        cy.contains('Camisa Sarja Cropped Color').click({waitForAnimations: false}) // clicando em um item
+        cy.wait(30000) 
+        cy.get('.lojausereserva-add-to-cart-button-0-x-buttonText').click({ force: true }); // clicando no botão para adicionar ao carrinho
         cy.wait(6000)
-        cy.get('.lojausereserva-minicart-2-x-openIconContainer').click()
+        cy.get('.lojausereserva-minicart-2-x-openIconContainer').click() // clicando em finalizar a compra
         cy.wait(10000)
-        cy.get(".minicart-footer__checkout-btn").click()
+        cy.get(".minicart-footer__checkout-btn").click() // clicando no carrinho
         cy.get('#payment-group-creditCardPaymentGroup > .payment-group-item-text').click({force: true})
       
     });
